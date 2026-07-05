5019e317599263817415ed1905b8f6f08b23d0b8
wanto-production <ikwansatria3974@gmail.com>
2026-07-05 12:40:33 +0700

    update

--- flake.nix
+++ flake.nix
@@ -4,29 +4,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    fenix,
-    crane,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        craneLib = crane.lib.${system};
-        rustNightly = fenix.packages.${system}.latest;
+
+        version = "1.0.0";
+
+        debUrl = "https://github.com/ikhwan-satrio/gibterm/releases/download/v${version}/gibterm_${version}_amd64.deb";
+
+        deb = pkgs.fetchurl {
+          url = debUrl;
+          hash = ""; # isi hash nya setelah build pertama
+        };
 
         runtimeDeps = with pkgs; [
           webkitgtk_4_1
@@ -38,117 +35,68 @@
           openssl
           sqlite
         ];
-
-        buildTools = with pkgs; [
-          pkg-config
-          bun
-          nodejs
-          glib
-          gsettings-desktop-schemas
-          hicolor-icon-theme
-        ];
-
-        rustToolchain = rustNightly.withComponents [
-          "cargo"
-          "clippy"
-          "rust-src"
-          "rustfmt"
-        ];
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = runtimeDeps ++ [rustToolchain];
-          nativeBuildInputs = buildTools ++ [
-            pkgs.wrapGAppsHook4
-            pkgs.cargo-tauri
+          buildInputs = runtimeDeps;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            bun
+            nodejs
+            wrapGAppsHook4
+            glib
+            gsettings-desktop-schemas
+            hicolor-icon-theme
           ];
-
-          shellHook = ''
-            export RUST_SRC_PATH="${rustToolchain.rustPlatform.rustLibSrc}"
-            export GSETTINGS_SCHEMA_DIR="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas"
-          '';
         };
 
-        packages.default = let
-          src = ./.;
-          cargoDeps = craneLib.importCargoLock {
-            lockFile = ./src-tauri/Cargo.lock;
-          };
-          cargoConfig = pkgs.writeText "cargo-config.toml" ''
-            [net]
-            offline = true
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "gibterm";
+          inherit version;
 
-            [source.vendored-sources]
-            directory = "${cargoDeps}"
+          src = deb;
 
-            [source.crates-io]
-            replace-with = "vendored-sources"
+          nativeBuildInputs = with pkgs; [
+            dpkg
+            autoPatchelfHook
+          ];
+
+          buildInputs = runtimeDeps;
+
+          dontConfigure = true;
+          dontBuild = true;
+
+          installPhase = ''
+            runHook preInstall
+
+            dpkg -x $src unpacked
+
+            mkdir -p $out
+            cp -r unpacked/usr/* $out/
+
+            # rewrite desktop file Exec path
+            if [ -f $out/share/applications/gibterm.desktop ]; then
+              sed -i "s|Exec=.*|Exec=$out/bin/gibterm|" $out/share/applications/gibterm.desktop
+            fi
+
+            runHook postInstall
           '';
-          icons = ./src-tauri/icons;
-        in
-          pkgs.stdenv.mkDerivation {
-            pname = "gibterm";
-            version = "1.0.0";
 
-            nativeBuildInputs = buildTools ++ [rustToolchain];
-            buildInputs = runtimeDeps;
+          preFixup = ''
+            gappsWrapperArgs+=(
+              --prefix GDK_BACKEND : "wayland:x11"
+              --prefix XDG_CURRENT_DESKTOP : "GNOME"
+              --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath runtimeDeps}"
+            )
+          '';
 
-            buildCommand = ''
-              export HOME=$(mktemp -d)
-              export CARGO_HOME="$HOME/.cargo"
-              export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-
-              mkdir -p "$CARGO_HOME"
-              cp ${cargoConfig} "$CARGO_HOME/config.toml"
-
-              cp -r ${src}/* .
-              chmod -R u+w .
-
-              bun install --frozen-lockfile
-              bun run build
-
-              cd src-tauri
-
-              cargo build --release --target-dir "$HOME/target"
-
-              mkdir -p $out/bin
-              cp "$HOME/target/release/gibterm" $out/bin/gibterm
-
-              mkdir -p $out/share/applications
-              cat > $out/share/applications/gibterm.desktop << 'DESKTOP_EOF'
-              [Desktop Entry]
-              Name=Gibterm
-              Exec=gibterm
-              Type=Application
-              Terminal=false
-              Categories=Development;Utility;TerminalEmulator;
-              Comment=Terminal emulator
-              DESKTOP_EOF
-
-              mkdir -p $out/share/icons/hicolor/128x128/apps
-              mkdir -p $out/share/icons/hicolor/256x256/apps
-              cp ${icons}/128x128.png $out/share/icons/hicolor/128x128/apps/gibterm.png
-              cp ${icons}/128x128@2x.png $out/share/icons/hicolor/256x256/apps/gibterm.png
-
-              chmod +x $out/bin/gibterm
-              patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-                --set-rpath "${pkgs.lib.makeLibraryPath runtimeDeps}" \
-                $out/bin/gibterm
-
-              wrapProgram $out/bin/gibterm \
-                --prefix GDK_BACKEND : "wayland:x11" \
-                --prefix XDG_CURRENT_DESKTOP : "GNOME" \
-                --prefix GSETTINGS_SCHEMA_DIR : "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas" \
-                --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeDeps}"
-            '';
-
-            meta = with pkgs.lib; {
-              description = "Terminal emulator";
-              homepage = "https://github.com/ikhwan-satrio/gibterm";
-              license = licenses.mit;
-              platforms = platforms.linux;
-              mainProgram = "gibterm";
-            };
+          meta = with pkgs.lib; {
+            description = "Terminal emulator";
+            homepage = "https://github.com/ikhwan-satrio/gibterm";
+            license = licenses.mit;
+            platforms = ["x86_64-linux"];
+            mainProgram = "gibterm";
           };
+        };
       }
     );
 }
